69dfb093b60cb4691426aba35be85ed0061220bb
cfv7 <hq@colin.red>
2017-03-30 12:18:14 -0400

    GET 2.0

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  DEV: {
+    client: 'pg',
+    //connection:'postgres://ghnufhnx:qI4AZzMvCoiaB-L0ty990gL85h4p-723@stampy.db.elephantsql.com:5432/ghnufhnx',
+    connection: {
+      user: 'dev',
+      password: 'letmein',
+      database: 'todo-list'
+    },
+    debug: true
+  },
+  TEST: {
+    client: 'pg',
+    //connection:'postgres://ghnufhnx:qI4AZzMvCoiaB-L0ty990gL85h4p-723@stampy.db.elephantsql.com:5432/ghnufhnx',
+    connection: {
+      user: 'dev',
+      password: 'letmein',
+      database: 'todo-list'
+    },
+    debug: true
+  },
+  PROD: {},
+  PORT: process.env.PORT || 8080  
+};
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-var express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {DEV} = require('./config');
+const knex = require('knex')(DEV);
+const app = express();
 
-var app = express();
-
+app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header({
     "Access-Control-Allow-Origin": "*",
@@ -13,10 +16,14 @@ app.use(function(req, res, next) {
 
 
 app.get('/', (req, res) => {
-  res.json([])
+  // res.json([]);
+  knex.select('title', 'completed', 'id', 'url')
+  .from('todos')
+  .then(results => res.json(results));
 });
 
-app.post('/', (req, res) => {
+
+app.post('/todo', (req, res) => {
   res.json({'title':'a todo'});
 });
 
@@ -24,9 +31,13 @@ app.delete('/', (req, res) => {
   res.json({});
 });
 
-app.post('/', (req, res) => {
-  res.json([{'title':'a todo'}]);
-});
+// app.post('/', (req, res) => {
+//   knex.select('title', 'completed', 'id', 'url')
+//   .from('todos')
+//   .then(results => res.json(results));
+// });
+
+
 
 app.listen(process.env.PORT || 8080);
 
